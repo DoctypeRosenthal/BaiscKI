62272ca465c9159e864bba82677884a6ad69c4e3
lorenz <lorenz.rosenthal@gmail.com>
2017-05-31 12:43:54 +0200

    Babel-presets :: moved babel configuration from .babelrc to webpack.config. Added stage-2 presets.

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,10 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
-                loaders: ['babel'],
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'es2017', 'stage-2']
+                },
                 exclude: /node_modules/
             },
             {
