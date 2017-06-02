7dea1e2a4512656b9e5502d73fa12aefae7cb659
lorenz <lorenz.rosenthal@gmail.com>
2017-06-02 18:01:55 +0200

    BinaryTree :: improved markup and style

--- dev/js/components/BinaryTree.js
+++ dev/js/components/BinaryTree.js
@@ -1,19 +1,12 @@
 import React from 'react'
 export default function BinaryTree({words}) {
+	if (!words) return <div className="bt-placeholder">&nbsp;</div>
 	return <div className="bt">
-		<div>
-			<div>{words.str}</div>
-			<span>{words.predictions.map((x,i) => <i key={i} className="bt-prediction">{x}</i>)}</span>
-		</div>
-		<div className="bt-child">{
-			!words.lower ? null :
+		<div>{words.str}</div>
+		<span>{words.predictions.map((x,i) => <i key={i} className="bt-prediction">{x}</i>)}</span>
+		<div className="bt-children">
 			<BinaryTree words={words.lower} /> 
-		}
-		</div>
-		<div className="bt-child">{
-			!words.higher ? null :
 			<BinaryTree words={words.higher} /> 
-		}
 		</div>
 	</div>
 }
